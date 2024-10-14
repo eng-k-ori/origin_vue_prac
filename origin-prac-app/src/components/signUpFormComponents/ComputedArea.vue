6c315b1305dd9a85e707f1c2c514b395c4712999
eng-k-ori <172562164+eng-k-ori@users.noreply.github.com>
2024-10-14 12:20:50 +0900

    ComputedAreaを作成

---
+++ origin-prac-app/src/components/signUpFormComponents/ComputedArea.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div>
+  <h3>ここは計算エリア</h3>
+  <input type="number" :value="props.firstInputNum" @input="updateFirstInputNum"/>
+  +
+  <input type="number" :value="props.secondInputNum" @input="updateSecondInputNum"/>
+  =
+  <input type="number" :value="props.sumNum" readonly/>
+</div>
+</template>
+
+<script setup lang="ts">
+interface computedAreaProps {
+  firstInputNum: number,
+  secondInputNum: number,
+  sumNum: number
+}
+interface computedAreaEmits {
+  (e: 'update:firstInputNum', value: number):void;
+  (e: 'update:secondInputNum', value: number):void;
+}
+
+const props = defineProps<computedAreaProps>()
+const emit = defineEmits<computedAreaEmits>()
+
+function updateFirstInputNum(e:Event){
+  const emitValue = e.target as HTMLInputElement;
+  emit('update:firstInputNum', Number(emitValue.value))
+}
+
+function updateSecondInputNum(e:Event){
+  const emitValue = e.target as HTMLInputElement;
+  emit('update:secondInputNum', Number(emitValue.value))
+}
+
+</script>
+
+<style scoped></style>
--- origin-prac-app/src/pages/RegistForm.vue
+++ origin-prac-app/src/pages/RegistForm.vue
@@ -1,23 +1,56 @@
 <template>
+  <!-- @update:userName="userName = $event -->
   <SignUpForm
     :userName="userName"
     :password="password"
-    @update:userName="userName = $event"
-    @update:password="password = $event"
+    @update:userName="updateUserName"
+    @update:password="updatePassword"
     @register="register"
     @clear="clear"
   />
   <UserList :userInfos="userInfos"/>
+  <ComputedArea
+    :firstInputNum="firstInputNum"
+    :secondInputNum="secondInputNum"
+    :sumNum="sumNum"
+    @update:firstInputNum="updateFirstInputNum"
+    @update:secondInputNum="updateSecondInputNum"
+  />
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 import SignUpForm from '@/components/signUpFormComponents/SignUpForm.vue';
 import UserList from '@/components/signUpFormComponents/UserList.vue';
+import ComputedArea from '@/components/signUpFormComponents/ComputedArea.vue';
 
 const userName = ref<string>('')
 const password = ref('')
 const userInfos = ref<Array<string>>([])
+
+const firstInputNum = ref<number>(0)
+const secondInputNum = ref<number>(0)
+
+// userNameを更新する関数
+function updateUserName(emitValue: string) {
+  userName.value = emitValue;
+}
+// passwordを更新する関数
+function updatePassword(emitValue: string) {
+  password.value = emitValue;
+}
+
+function updateFirstInputNum(emitValue: number) {
+  firstInputNum.value = emitValue;
+}
+
+function updateSecondInputNum(emitValue: number) {
+  secondInputNum.value = emitValue;
+}
+// ロジックの実装は親なので、親側で記載。
+const sumNum = computed(() => {
+  return firstInputNum.value + secondInputNum.value
+})
 
 function register(){
   userInfos.value.push(userName.value)
@@ -33,5 +66,6 @@ function clear(){
 </script>
 <style scoped>
 form {background-color: limegreen;}
+div {background-color:  lightpink;}
 
 </style>
